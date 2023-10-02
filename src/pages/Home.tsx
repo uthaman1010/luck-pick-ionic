1f6ed87798f4b0401da986b063915a8a40c92123
UTHAMAN MUTHAIAH <uthaman1234@UTHAMANs-Mac-mini.local>
2023-10-02 09:26:09 +0800

    setup ionic react project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>FreeCodeCamp</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
